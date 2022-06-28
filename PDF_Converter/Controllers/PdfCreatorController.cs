cb80ef58dfbf42940cff22ae26703db800c8234a
adayesi <abah.edward@decagon.dev>
2022-06-28 18:29:25 +0100

    complete commit

---
+++ PDF_Converter/Controllers/PdfCreatorController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using PDF_Converter.Utility;
+
+namespace PDF_Converter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfCreatorController : ControllerBase
+    {
+        private readonly IConverter _converter;
+
+        public PdfCreatorController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        [HttpGet]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings {Top = 10},
+                DocumentTitle = "PDF Report",
+                //Out = @"C:\Users\hp\Documents\AAA\Employee_Report.pdf"  //USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION.
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                Page = "https://code-maze.com/", //USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
+                WebSettings =
+                {
+                    DefaultEncoding = "utf-8",
+                    UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css")
+                },
+                HeaderSettings = {FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true},
+                FooterSettings = {FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer"}
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            _converter.Convert(pdf); //IF WE USE Out PROPERTY IN THE GlobalSettings CLASS, THIS IS ENOUGH FOR CONVERSION
+
+            var file = _converter.Convert(pdf);
+
+            //return Ok("Successfully created PDF document."); // to save pdf file on local system/storage. To use this, uncomment the "out" value in the global settings above.
+
+            //return File(file, "application/pdf", "EmployeeReport.pdf"); //to download pdf file.
+
+            return File(file, "application/pdf"); // to display pdf file on browser 
+        }
+    }
+}
